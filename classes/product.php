a48f09fb7758464a09bbb7e5a27546d9fa6c66ed
ayumi <dottoz2531@gmail.com>
2024-07-13 18:14:46 +0900

    PHP④

---
+++ classes/product.php
@@ -0,0 +1,34 @@
+<?php
+  // Class というのは処理や値をまとまりにしたもの
+  class Product {
+    private $id;
+    private $name; // private にするとインスタンスからアクセスできない
+    private $price;
+    private $image;
+
+    public function __construct($id, $name, $price, $image) {
+      $this->id = $id;
+      $this->name = $name;
+      $this->price = $price;
+      $this->image = $image;
+    }
+
+    public function getName() {
+      return $this->name;
+    }
+
+    public function getPrice() {
+      return $this->price;
+    }
+
+    public function getImage() {
+      return $this->image;
+    }
+
+    public function getId() {
+      return $this->id;
+    }
+
+  }
+  // construct は class から インスタンス を作るときに使う
+?>
